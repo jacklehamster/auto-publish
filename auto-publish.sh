7e1154a9f71c929f6d59ab66e8e44a8d54c376b9
Jack Le Hamster <jacklehamster@gmail.com>
2024-02-27 18:34:13 -0800

    Add auto-commit

--- auto-publish.sh
+++ auto-publish.sh
@@ -4,7 +4,8 @@
 script_dir="$(pwd)"
 target_directory="${1:-.}"
 
-echo "run script > $script_dir/make-repo-ssh.sh from $target_directory"
+# echo "run script > $script_dir/make-repo-ssh.sh from $target_directory"
 
 cd "$target_directory"
 sh "$script_dir/make-repo-ssh.sh"
+npm explore @dobuki/autocommit -- bun run autocommit \"$(pwd)\"
