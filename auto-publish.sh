97cd4bfe529351cfa1fafe9b917139f8a1c54e9b
Jack Le Hamster <jacklehamster@gmail.com>
2024-02-27 16:53:34 -0800

    commit

---
+++ auto-publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the target directory to the provided argument, or use the current directory
+script_dir="$(pwd)"
+target_directory="${1:-.}"
+
+echo "run script > $script_dir/make-repo-ssh.sh from $target_directory"
+
+cd "$target_directory"
+sh "$script_dir/make-repo-ssh.sh"
